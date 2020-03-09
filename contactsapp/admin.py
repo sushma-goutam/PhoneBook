10aa2d99ff161a8407f020fc00c24d29eab95e8e
Sushma Goutam <sushma.goutam@gmail.com>
2020-03-09 16:57:24 +0100

    A working implementation of creating a wheel for Django project. The templates and static folder are also copied and working fine.

--- contactsapp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from contactsapp.models import Contact
-
-admin.site.register(Contact)
---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from setuptools import setup
+
+version = "1.0.0"
+
+if sys.argv[-1] == 'publish':
+    os.system('make release')
+    sys.exit()
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    readme = f.read()
+
+description = "Phone book using Django REST Framework."
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+server_packages = ['src', 'src.PhoneBook', 'src.contactsapp']
+server_package_data = {'': ['*.py', '*.sqlite3', 'README.md']}
+
+setup(
+    name='PhoneBook',
+    version=version,
+    description=description,
+    long_description=readme,
+    author='Sushma Goutam',
+    author_email='sushma.goutam@gmail.com',
+    url='https://github.com/sushma-goutam/PhoneBook',
+    packages=server_packages,
+    package_dir={'PhoneBook': 'PhoneBook'},
+    package_data=server_package_data,
+    include_package_data=True,
+    #data_files=[('Lib//site-packages', ['db.sqlite3', 'manage.py'])],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'Django>=2.2,<3.1;python_version>="3.7"',
+    ],
+    license="BSD",
+    zip_safe=False,
+    #py_modules=['src/PhoneBook/__main__'],
+    py_modules=['src/manage'],
+    keywords='PhoneBook',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+    ],
+)
