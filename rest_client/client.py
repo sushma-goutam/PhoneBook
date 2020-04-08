aaafe919c685c969c2f3c831fde9a1a3fcbfa232
sushma-goutam <sushma.goutam@perkinelmer.com>
2020-04-08 19:18:46 +0200

    Post Rest API data using requests library

--- rest_client/client.py
+++ rest_client/client.py
@@ -1,9 +1,12 @@
 import requests
 
 
+# This will help in case we need to change url later
+URL = "http://127.0.0.1:8000"
+
 def get_token():
     # Get auth token
-    url = "http://127.0.0.1:8000/api/auth/"
+    url = f"{URL}/api/auth/"
     response = requests.post(url,
                              data={'username': 'admin',
                                    'password': 'admin'}
@@ -13,9 +16,8 @@ def get_token():
     return response.json()
 
 
-def get_data():
-    # Get auth token
-    url = "http://127.0.0.1:8000/api/contact_list/"
+def get_contact():
+    url = f"{URL}/api/contact_list/"
     header = {'Authorization': f'Token {get_token()}'}
     response = requests.get(url, headers=header)
 
@@ -24,4 +26,23 @@ def get_data():
         print(c)
 
 
-get_data()
+def create_contact(count):
+    url = f"{URL}/api/contact_list/"
+    data = {
+        "id": count,
+        "first_name": "Mayank",
+        "last_name": "Nayak",
+        "phone_number": "234567"
+    }
+    header = {'Authorization': f'Token {get_token()}'}
+    response = requests.post(url, data=data, headers=header)
+    print(response.text)
+
+
+def create_many_contacts(count):
+    """ This is useful in feeding data from excel to django db """
+    for i in range(1, count + 1):
+        create_contact(i)
+
+
+create_contact()
